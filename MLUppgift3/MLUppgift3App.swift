a31e333432e665f735829736d04e51a165161418
Carl-Johan Heinze <cj.heinze@mac.com>
2023-10-09 11:13:51 +0200

    Initial Commit

---
+++ MLUppgift3/MLUppgift3App.swift
@@ -0,0 +1,17 @@
+//
+//  MLUppgift3App.swift
+//  MLUppgift3
+//
+//  Created by Carl-Johan Heinze on 2023-10-09.
+//
+
+import SwiftUI
+
+@main
+struct MLUppgift3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
